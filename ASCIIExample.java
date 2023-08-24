1b2107332ffa2cd49a4f4688c345821f2c87eeff
Oluwabunmi Odusola <esther.prismcheck@gmail.com>
2023-08-24 10:17:14 -0400

    first commit

---
+++ ASCIIExample.java
@@ -0,0 +1,12 @@
+package com.bptn.course.week1;
+
+public class ASCIIExample {
+
+	public static void main(String[] args) {
+		char b = 'd';
+		int ascii = b;
+		System.out.println("The ASCII value" + b + " is: " + ascii);
+
+	}
+
+}
